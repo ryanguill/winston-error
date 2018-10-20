22c09fdfad308cb1ff3f0bd4096940e544897482
Ryan Guill <ryanguill@gmail.com>
2018-10-20 13:44:07 -0500

    WIP

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-'use strict';
+"use strict";
 
-var _ = require('lodash');
+var clone = require("lodash.clone");
+var pick = require("lodash.pick");
+var partial = require("lodash.partial");
+var defaults = require("lodash.defaults");
+var keys = require("lodash.keys");
+var drop = require("lodash.drop");
 
 /**
  * Expose module.
  */
 
 module.exports = function winstonLoggerDecorator(logger, options) {
-
-  var originalLogger = _.clone(logger); // not deep
+  var originalLogger = clone(logger); // not deep
 
   options = options || {};
-  options.decoratedLevels = options.decoratedLevels || [
-    'error'
-  ];
+  options.decoratedLevels = options.decoratedLevels || ["error"];
   options.pickedFields = options.pickedFields || {
     name: undefined,
     message: undefined,
     stack: undefined
   };
-  var pickedKeys = _.keys(options.pickedFields);
+  var pickedKeys = keys(options.pickedFields);
 
   function winstonCallRewriter(loggerMethod, message, metadata) {
-
-    if (! (message instanceof Error)) {
+    if (!(message instanceof Error)) {
       // this decorator isn't needed
-      return loggerMethod.apply(logger, _.drop(arguments, 1));
+      return loggerMethod.apply(logger, drop(arguments, 1));
     }
 
     var error = message;
 
     // Keep original metadata safe.
-    metadata = _.clone(metadata || {});
+    metadata = clone(metadata || {});
 
     // Copy only whitelisted error fields in metadata,
     // providing an optional default value
-    metadata.error = _.defaults(
-      _.pick(error, pickedKeys),
-      options.pickedFields
-    );
+    metadata.error = defaults(pick(error, pickedKeys), options.pickedFields);
 
     // Replace message by error message.
     message = error.message;
 
     // Log with arguments re-arranged.
-    var args = [message, metadata].concat(_.drop(arguments, 3));
+    var args = [message, metadata].concat(drop(arguments, 3));
     loggerMethod.apply(logger, args);
   }
 
   options.decoratedLevels.forEach(function(level) {
-    logger[level] = _.partial(winstonCallRewriter, originalLogger[level]);
+    logger[level] = partial(winstonCallRewriter, originalLogger[level]);
   });
 };
